654e5d1c89a37fbd38005343d911194f5a8cf7c7
David Levy <lennox.davidlevy@gmail.com>
2022-11-09 23:47:51 -0500

    working

---
+++ lua/user/lsp/settings/html.lua
@@ -0,0 +1,23 @@
+return {
+  filetypes = {
+    "html",
+    "css",
+    "scss",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "haml",
+    "xml",
+    "xsl",
+    "pug",
+    "slim",
+    "sass",
+    "stylus",
+    "less",
+    "sss",
+    "hbs",
+    "handlebars",
+  },
+}
+
